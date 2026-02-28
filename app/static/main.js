a3b18f1c39801bd3766f6f226779193b0ccd77c8
dongmin <happydm09@naver.com>
2026-02-28 20:17:48 +0900

    Update main.js

--- app/static/main.js
+++ app/static/main.js
@@ -23,12 +23,14 @@ async function start() {
   var url = document.querySelector('#url').value
   var host = url.split('://')[1].replace('/', '')
   
-  var res = await getAPI('/api/get_mi?host='+host)
   var el = document.querySelector('#link')
+  el.innerHTML = '<가져오는 중> <br><br>'
+  
+  var res = await getAPI('/api/get_mi?host='+host)
   
   if (res == 'error') {
     el.innerHTML = '< URL이 올바르지 않거나 지원하지 않는 학교입니다. > <br><br>'
   } else {
-    el.innerHTML = `ics 링크: https://haksamo.vercel.app/cal?host=${host}&mi=${res} <br><br>`
+    el.innerHTML = `ics 링크: https://haksamo.vercel.app/cal?host=${host}&mi=${res} <br><a href='#' onClick="window.open('https://share.google/aimode/EBAccMJ5ViUuWCyxP')">캘린더에 추가하는 방법</a><br><br>`
   }
 }
